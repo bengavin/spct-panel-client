cb190838879a96a69b73689f420120d277d08e38
Ben Gavin <ben_gavin@trekbikes.com>
2018-06-06 23:07:24 -0500

    UPDATE:  Implement command parsing/handling

--- SPCTLightPanelClient.ino
+++ SPCTLightPanelClient.ino
@@ -7,6 +7,7 @@
 #define LF 10
 
 #define COMMAND_BUFFER_LEN 100
+
 #define CMD_HELLO_LEN 6
 #define CMD_HELLO "HELLO"
 #define CMD_GOODBYE_LEN 7
@@ -18,17 +19,26 @@
 #define CMD_PLAY_LEN 4
 #define CMD_PLAY "PLAY"
 #define CMD_RESET_LEN 5
+#define CMD_RESET "RESET"
 #define CMD_STOP_LEN 4
 #define CMD_STOP "STOP"
-#define CMD_SETCOLOR_LEN 8
-#define CMD_SETCOLOR "SETCOLOR"
+#define CMD_SET_COLOR_LEN 8
+#define CMD_SET_COLOR "SETCOLOR"
 #define CMD_ON_LEN 2
 #define CMD_ON "ON"
 #define CMD_OFF_LEN 3
 #define CMD_OFF "OFF"
 
+void processUnknownCommand(AltSoftSerial *controller);
 void processHelloCommand(AltSoftSerial *controller);
 void processGoodbyeCommand(AltSoftSerial *controller);
+void processCommandPanelOff(AltSoftSerial *controller);
+void processSetTempoCommand(AltSoftSerial *controller, char *args);
+void processResetCommand(AltSoftSerial *controller, char *args);
+void processStopCommand(AltSoftSerial *controller, char *args);
+void processSetColorCommand(AltSoftSerial *controller, char *args);
+void processSetLedOnCommand(AltSoftSerial *controller, char *args);
+void processSetLedOffCommand(AltSoftSerial *controller, char *args);
 
 AltSoftSerial controller;
 
@@ -80,18 +90,43 @@ static void readSerialToCommandBuffer() {
 }
 
 static void resetCommandBuffer() {
+  commandComplete = false;
   commandLen = 0;
-  commandBuffer[0] = '\0';
+  for(int i = 0; i < COMMAND_BUFFER_LEN && commandBuffer[i] != '\0'; i++) { commandBuffer[i] = '\0'; }
 }
 
 static void processCommand() {
   if (strncmp(CMD_HELLO, commandBuffer, CMD_HELLO_LEN) == 0) {
-    resetCommandBuffer();
     processHelloCommand(&controller);
   }
   else if (strncmp(CMD_GOODBYE, commandBuffer, CMD_GOODBYE_LEN) == 0) {
-    resetCommandBuffer();
     processGoodbyeCommand(&controller);
   }
+  else if (strncmp(CMD_PANEL_OFF, commandBuffer, CMD_PANEL_OFF_LEN) == 0) {
+    processCommandPanelOff(&controller);
+  }
+  else if (strncmp(CMD_SET_TEMPO, commandBuffer, CMD_SET_TEMPO_LEN) == 0) {
+    processSetTempoCommand(&controller, &commandBuffer[CMD_SET_TEMPO_LEN]);
+  }
+  else if (strncmp(CMD_RESET, commandBuffer, CMD_RESET_LEN) == 0) {
+    processResetCommand(&controller, &commandBuffer[CMD_RESET_LEN]);
+  }
+  else if (strncmp(CMD_STOP, commandBuffer, CMD_STOP_LEN) == 0) {
+    processStopCommand(&controller, &commandBuffer[CMD_STOP_LEN]);
+  }
+  else if (strncmp(CMD_SET_COLOR, commandBuffer, CMD_SET_COLOR_LEN) == 0) {
+    processSetColorCommand(&controller, &commandBuffer[CMD_SET_COLOR_LEN]);
+  }
+  else if (strncmp(CMD_ON, commandBuffer, CMD_ON_LEN) == 0) {
+    processSetLedOnCommand(&controller, &commandBuffer[CMD_ON_LEN]);
+  }
+  else if (strncmp(CMD_OFF, commandBuffer, CMD_OFF_LEN) == 0) {
+    processSetLedOffCommand(&controller, &commandBuffer[CMD_OFF_LEN]);
+  }
+  else {
+    processUnknownCommand(&controller);
+  }
+  
+  resetCommandBuffer();
 }
 
--- SPCTLightPanelCommands.ino
+++ SPCTLightPanelCommands.ino
@@ -1,3 +1,53 @@
+int extractArgs(String *outBuffer, size_t outBufferLength, char *args) {
+  String argString(args);
+  int lastSpaceIndex = 0;
+  
+  for(int i = 0; i < outBufferLength && lastSpaceIndex >= 0; i++) {
+    int spaceIndex = argString.indexOf(' ', lastSpaceIndex +1);
+    Serial.print("Space Index: ");
+    Serial.print(spaceIndex, DEC);
+    Serial.print(" Last Space Index: ");
+    Serial.println(lastSpaceIndex, DEC);
+    
+    if (spaceIndex == -1) {
+      outBuffer[i] = argString.substring(lastSpaceIndex <= 0 ? lastSpaceIndex : (lastSpaceIndex + 1));
+    }
+    else {
+      outBuffer[i] = argString.substring(lastSpaceIndex, spaceIndex);
+    }
+    lastSpaceIndex = spaceIndex;
+  }
+
+  return lastSpaceIndex;
+}
+
+int extractArgs(int *outBuffer, size_t outBufferLength, char *args) {
+  String stringBuffer[outBufferLength];
+  int retVal = extractArgs(stringBuffer, outBufferLength, args);
+
+  for(int i = 0; i < outBufferLength && stringBuffer[i] != NULL; i++) {
+      outBuffer[i] = stringBuffer[i].toInt();
+  }
+
+  return retVal;
+}
+
+int extractArgs(float *outBuffer, size_t outBufferLength, char *args) {
+  String stringBuffer[outBufferLength];
+  int retVal = extractArgs(stringBuffer, outBufferLength, args);
+
+  for(int i = 0; i < outBufferLength && stringBuffer[i] != NULL; i++) {
+      outBuffer[i] = stringBuffer[i].toFloat();
+  }
+
+  return retVal;
+}
+
+void processUnknownCommand(AltSoftSerial *controller) {
+  Serial.println("Unrecognized command received");
+  controller->println("ERROR");
+}
+
 void processHelloCommand(AltSoftSerial *controller) {
     Serial.println("Received HELLO command, turning on LED");
     
@@ -19,3 +69,149 @@ void processGoodbyeCommand(AltSoftSerial *controller) {
     Serial.println("Sending SHUTDOWN response");
     controller->println("SHUTDOWN");
 }
+
+void processCommandPanelOff(AltSoftSerial *controller) {
+  Serial.println("Turning off panel");
+
+  // TODO - turn panel off
+  
+  controller->println("OFF");
+}
+
+void processSetTempoCommand(AltSoftSerial *controller, char *args) {
+  String argString(args);
+  int tempo = argString.toInt();
+
+  Serial.print("Setting panel tempo to ");
+  Serial.println(tempo, DEC);
+  
+  // TODO - set tempo to value
+  controller->print("TEMPO ");
+  controller->println(tempo, DEC);
+}
+
+void processResetCommand(AltSoftSerial *controller, char *args) {
+  String argString(args);
+  int row = argString.toInt();
+
+  Serial.print("Resetting row");
+  Serial.println(row, DEC);
+  
+  // TODO - set tempo to value
+  controller->print("OK ");
+  controller->println(row, DEC);
+}
+
+void processStopCommand(AltSoftSerial *controller, char *args) {
+  int argInts[2];
+  extractArgs(argInts, 2, args);
+
+  Serial.print("Stopping Requested Row Display: ");
+  Serial.println(argInts[0], DEC);
+
+  // TODO - stop the pattern
+
+  if (argInts[1] == 1) {
+    Serial.println("Resetting Stopped Row");
+
+    // TODO - reset the pattern
+  }
+  
+  controller->print("STOPPED ");
+  controller->println(argInts[0], DEC);
+}
+
+void processSetColorCommand(AltSoftSerial *controller, char *args) {
+  int argInts[] = {-1, -1, -1, -1, -1};
+  int lastIndex = extractArgs(argInts, 5, args);
+  float argFloats[] = { -1.0 };
+  if (lastIndex >= 0) {
+    extractArgs(argFloats, 1, &args[lastIndex]);
+  }
+
+  Serial.print("Setting Color On Requested LED (");
+  Serial.print(argInts[0], DEC);
+  Serial.print(",");
+  Serial.print(argInts[1], DEC);
+  Serial.print(") to RGBA (");
+  Serial.print(argInts[2], DEC);
+  Serial.print(",");
+  Serial.print(argInts[3], DEC);
+  Serial.print(",");
+  Serial.print(argInts[4], DEC);
+  Serial.print(",");
+  Serial.print(argFloats[0], DEC);
+  Serial.println(")");
+
+  // TODO - Set the color
+
+  controller->println("DONE");  
+}
+
+void processSetLedOnCommand(AltSoftSerial *controller, char *args) {
+  int argInts[] = {-1, -1, -1, -1, -1};
+  int lastIndex = extractArgs(argInts, 5, args);
+  float argFloats[] = { -1.0 };
+  if (lastIndex >= 0) {
+    extractArgs(argFloats, 1, &args[lastIndex]);
+  }
+
+  Serial.print("Turning On Requested LED (");
+  Serial.print(argInts[0], DEC);
+  Serial.print(",");
+  Serial.print(argInts[1], DEC);
+  Serial.println(")");
+
+  // TODO - Turn on the LED
+
+  if (argInts[2] >= 0) {
+    Serial.print("Setting LED Color RGBA (");
+    Serial.print(argInts[2], DEC);
+    Serial.print(",");
+    Serial.print(argInts[3], DEC);
+    Serial.print(",");
+    Serial.print(argInts[4], DEC);
+    Serial.print(",");
+    Serial.print(argFloats[0], DEC);
+    Serial.println(")");
+
+    // TODO - Set LED Color
+  }
+
+  controller->println("DONE");  
+}
+
+void processSetLedOffCommand(AltSoftSerial *controller, char *args) {
+  int argInts[] = {-1, -1, -1, -1, -1};
+  int lastIndex = extractArgs(argInts, 5, args);
+  float argFloats[] = { -1.0 };
+  if (lastIndex >= 0) {
+    extractArgs(argFloats, 1, &args[lastIndex]);
+  }
+
+  Serial.print("Turning Off Requested LED (");
+  Serial.print(argInts[0], DEC);
+  Serial.print(",");
+  Serial.print(argInts[1], DEC);
+  Serial.println(")");
+
+  // TODO - Turn off the LED
+
+  if (argInts[2] >= 0) {
+    Serial.print("Setting LED Color RGBA (");
+    Serial.print(argInts[2], DEC);
+    Serial.print(",");
+    Serial.print(argInts[3], DEC);
+    Serial.print(",");
+    Serial.print(argInts[4], DEC);
+    Serial.print(",");
+    Serial.print(argFloats[0], DEC);
+    Serial.println(")");
+
+    // TODO - Set LED Color
+  }
+
+  controller->println("DONE");
+}
+
+
